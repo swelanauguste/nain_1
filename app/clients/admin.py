2de80347b2b01699ee8e9e6a55ab469a76b28d9d
swelanauguste <swelanauguste@gmail.com>
2021-10-25 18:57:35 -0400

    added the user model to admin

--- app/clients/admin.py
+++ app/clients/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Client, Location
+from .models import Client, Location, User
 
 admin.site.register(Client)
 admin.site.register(Location)
+admin.site.register(User)
